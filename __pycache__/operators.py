69ab8f0410c89f1cc19a92cbb1acd4f6bf08e197
Rucheelt <narsingraosto@gmail.com>
2024-12-11 17:21:32 +0530

    arithmatic operators

---
+++ __pycache__/operators.py
@@ -0,0 +1,18 @@
+a=15
+b=3
+
+print(f"addition:{a+b}")
+print(f"subtraction:{a-b}")
+print(f"multiplication:{a*b}")
+print(f"division:{a/b}")
+print(f"double division:{a//b}")
+print(f"modulus:{a%b}")
+print(f"exponents:{a**b}")
+
+print(a==b)
+print(a<b)
+print(a>b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+
